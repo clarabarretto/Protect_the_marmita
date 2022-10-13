10c91a11e02bfb428dc5d8c50fa2ecc68b62ce3c
geydsonrenan <114956476+geydsonrenan@users.noreply.github.com>
2022-10-12 22:07:08 -0300

    Update main.py

--- main.py
+++ main.py
@@ -6,19 +6,20 @@ from random import randint
 
 pygame.init()
 space = False
-lar = 1080
+larg = 1080
 alt = 720
 points = 0
 fonte = pygame.font.SysFont('arial', 20, True, True)
 fonte_inicial = pygame.font.SysFont('arial', 3000, True, False)
-tela = pygame.display.set_mode((lar, alt))
-texto_inicial = fonte.render('AssaCInato!', False, (255,255,255))
+tela = pygame.display.set_mode((larg, alt))
+texto_inicial = fonte.render('AssaCInato!', False, (0,0,0))
 pygame.display.set_caption('AssaCInato')
 x = 300
 y = 580
-x2 = randint(10 , 1070)
-y2 = randint(10, 710)
+x2 = randint(20 , 1060)
+y2 = randint(20, 700)
 relogio = pygame.time.Clock()
+mapa = pygame.image.load('sprites/mapamundi.png')
 
 class Player(pygame.sprite.Sprite):
     def __init__(self, x, y):
@@ -32,23 +33,24 @@ class Player(pygame.sprite.Sprite):
         self.image = self.sprites[self.atual]
         self.rect = self.image.get_rect()
         self.rect.topleft = x, y
-        self.image = pygame.transform.scale(self.image, (24*2, 34*2))
+        self.image = pygame.transform.scale(self.image, (24, 34))
     def baixo(self, x, y):
         self.image = self.sprites[0]
-        self.image = pygame.transform.scale(self.image, (24*2, 34*2))
+        self.image = pygame.transform.scale(self.image, (24, 34))
         self.rect.topleft = x, y
     def cima(self, x, y):
         self.image = self.sprites[1]
-        self.image = pygame.transform.scale(self.image, (24*2, 34*2))
+        self.image = pygame.transform.scale(self.image, (24, 34))
         self.rect.topleft = x, y
     def direita(self, x, y):
         self.image = self.sprites[2]
-        self.image = pygame.transform.scale(self.image, (24*2, 34*2))
+        self.image = pygame.transform.scale(self.image, (24, 34))
         self.rect.topleft = x, y
     def esquerda(self, x, y):
         self.image = self.sprites[3]
-        self.image = pygame.transform.scale(self.image, (24*2, 34*2))
+        self.image = pygame.transform.scale(self.image, (24, 34))
         self.rect.topleft = x, y
+        
 
 todas_as_sprites = pygame.sprite.Group()
 
@@ -59,7 +61,8 @@ while True:
     relogio.tick(30)
     tela.fill((0,0,0))
     mensage = f"score : {points}"
-    text = fonte.render(mensage, False, (255, 255, 255))
+    text = fonte.render(mensage, False, (0, 0, 0))
+    tela.blit(mapa, (0,0))
     for event in pygame.event.get():
         if event.type == QUIT:
             pygame.quit()
@@ -68,13 +71,13 @@ while True:
             if event.key == K_a and x != 0:
                 x -= 5
                 player.esquerda(x, y)
-            if event.key == K_d and x != 1030:
+            if event.key == K_d and x != 1055:
                 x += 5
                 player.direita(x, y)
             if event.key == K_w and y != 0:
                 y -= 5
                 player.cima(x, y)
-            if event.key == K_s and y != 650:
+            if event.key == K_s and y != 685:
                 y += 5
                 player.baixo(x, y)
             if event.key == K_SPACE:
@@ -82,21 +85,23 @@ while True:
     if pygame.key.get_pressed()[K_a] and x != 0:
         x -= 5
         player.esquerda(x, y)
-    if pygame.key.get_pressed()[K_d] and x != 1030:
+    if pygame.key.get_pressed()[K_d] and x != 1055:
         x += 5
         player.direita(x, y)
     if pygame.key.get_pressed()[K_w] and y != 0:
         y -= 5
         player.cima(x, y)
-    if pygame.key.get_pressed()[K_s] and y != 650:
+    if pygame.key.get_pressed()[K_s] and y != 685:
         y += 5
         player.baixo(x, y)
-    ret2 = pygame.draw.rect(tela, (200, 0 , 0), (x2,y2, 80, 120))
-    #if ret1.colliderect(ret2):#
-       # points += 1#
-       # x2 = randint(80, 1010)#
-       #  y2 = randint(120, 600)#
-    if points == 50:
+    rect = pygame.draw.rect(tela,(200, 100, 0), (x2, y2, 20, 20))
+    for sprite in todas_as_sprites:
+        if sprite.rect.colliderect(rect):
+            x2 = randint(20, 1060)
+            y2 = randint(20, 700)
+            points += 1
+    if points == 10:
+        print('parabens caraio')
         quit()
     if y > 720:
         y = 0

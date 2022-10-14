cc684233489b610d28a6e51594f79b31b20a1fed
geydsonrenan <114956476+geydsonrenan@users.noreply.github.com>
2022-10-14 17:23:53 -0300

    Update main.py

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import pygame
 from pygame.locals import *
 from sys import exit 
 from random import randint
+import player as pl
 
 pygame.init()
 space = False
@@ -21,40 +22,9 @@ y2 = randint(20, 700)
 relogio = pygame.time.Clock()
 mapa = pygame.image.load('sprites/mapamundi.png')
 
-class Player(pygame.sprite.Sprite):
-    def __init__(self, x, y):
-        pygame.sprite.Sprite.__init__(self)
-        self.sprites = []
-        self.sprites.append(pygame.image.load('sprites/perso_baixo.png'))
-        self.sprites.append(pygame.image.load('sprites/perso_cima1.png'))
-        self.sprites.append(pygame.image.load('sprites/perso_direita.png'))
-        self.sprites.append(pygame.image.load('sprites/perso_esquerda.png'))
-        self.atual = 0
-        self.image = self.sprites[self.atual]
-        self.rect = self.image.get_rect()
-        self.rect.topleft = x, y
-        self.image = pygame.transform.scale(self.image, (24, 34))
-    def baixo(self, x, y):
-        self.image = self.sprites[0]
-        self.image = pygame.transform.scale(self.image, (24, 34))
-        self.rect.topleft = x, y
-    def cima(self, x, y):
-        self.image = self.sprites[1]
-        self.image = pygame.transform.scale(self.image, (24, 34))
-        self.rect.topleft = x, y
-    def direita(self, x, y):
-        self.image = self.sprites[2]
-        self.image = pygame.transform.scale(self.image, (24, 34))
-        self.rect.topleft = x, y
-    def esquerda(self, x, y):
-        self.image = self.sprites[3]
-        self.image = pygame.transform.scale(self.image, (24, 34))
-        self.rect.topleft = x, y
-        
-
 todas_as_sprites = pygame.sprite.Group()
 
-player = Player(x , y)
+player = pl.Player(x, y)
 todas_as_sprites.add(player)
 
 while True:

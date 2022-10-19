3661f295bc1d9d8bf7568a652c8b89ea0e26f442
Gabriel Aragão <97701396+gabrielaragao01@users.noreply.github.com>
2022-10-18 21:40:04 -0300

    Update drops.py

--- drops.py
+++ drops.py
@@ -4,16 +4,12 @@ from pygame.locals import *
 from random import randint
 
 def dropar(x, y):
-    chance = randint(1, 25)
-    lista_coordenadas_vida = [x, y]
+    chance = randint(1, 10)
+    lista_coordenadas = [x, y]
     if chance == 1:
         drop = True
     else:
         drop = False
 
-    lista_coordenadas_vida.append(drop)
-    return lista_coordenadas_vida
-    
-
-
-        
+    lista_coordenadas.append(drop)
+    return lista_coordenadas

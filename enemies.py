0a6300fdb21747de6b9cb69bcb4c172e63e843fb
geydsonrenan <114956476+geydsonrenan@users.noreply.github.com>
2022-10-20 00:37:19 -0300

    Update enemies.py

--- enemies.py
+++ enemies.py
@@ -1,5 +1,6 @@
 import pygame
 from pygame.locals import *
+from random import randint
 
 
 class Enemies(pygame.sprite.Sprite):
@@ -7,13 +8,18 @@ class Enemies(pygame.sprite.Sprite):
         pygame.sprite.Sprite.__init__(self)
         self.sprites = []
         self.sprites.append(pygame.image.load('sprites/monster1.png'))
+        self.numero = randint(1,5)
+        if self.numero <= 2:
+            self.escolha = 'player'
+        else:
+            self.escolha = 'geladeira'
         self.atual = 0
         self.image = self.sprites[self.atual]
         self.rect = self.image.get_rect()
         self.x = x
         self.y = y
         self.rect.topleft = self.x, self.y
-    def movimento(self):
+    def movimento(self, x, y):
         if self.y == 0:
             self.y += 2
         elif self.x == 0:
@@ -22,22 +28,53 @@ class Enemies(pygame.sprite.Sprite):
             self.y -= 2
         elif self.x == 1080:
             self.x -= 2
-        elif self.x == 560 and self.y < 350:
-            self.y += 2
-        elif self.x == 560 and self.y > 350:
-            self.y -= 2
-        elif self.y == 360 and self.x > 520:
-            self.x -= 2
-        elif self.y == 360 and self.x < 520:
-            self.x += 2
-        elif 470 <= self.x <= 580 and self.y < 350:
-            self.y += 2
-        elif 470 <= self.x <= 580 and self.y > 350:
-            self.y -= 2
-        elif 320 <= self.y <= 445 and self.x < 520:
-            self.x += 2
-        elif 320 <= self.y <= 445 and self.x > 520:
-            self.x -= 2
+        elif self.escolha == 'geladeira':
+            if self.x == 560 and self.y < 350:
+                self.y += 2
+            elif self.x == 560 and self.y > 350:
+                self.y -= 2
+            elif self.y == 360 and self.x > 520:
+                self.x -= 2
+            elif self.y == 360 and self.x < 520:
+                self.x += 2
+            elif 470 <= self.x <= 580 and self.y < 350:
+                self.y += 2
+            elif 470 <= self.x <= 580 and self.y > 350:
+                self.y -= 2
+            elif 320 <= self.y <= 445 and self.x < 520:
+                self.x += 2
+            elif 320 <= self.y <= 445 and self.x > 520:
+                self.x -= 2
+        elif self.escolha == 'player':
+            if self.x == x and self.y < y:
+                self.y += 2
+            elif self.x == x and self.y > y:
+                self.y -= 2
+            elif self.y == y and self.x > x:
+                self.x -= 2
+            elif self.y == y and self.x < x:
+                self.x += 2
+            elif x - 80 <= self.x <= x + 80 and self.y < y:
+                self.y += 2
+            elif x - 80 <= self.x <= x + 80 and self.y > y:
+                self.y -= 2
+            elif y - 80 <= self.y <= y + 80 and self.x < x:
+                self.x += 2
+            elif y - 80 <= self.y <= y + 80 and self.x > x:
+                self.x -= 2
+            else:
+                if self.x < x and self.y > y:
+                    self.x += 2
+                    self.y -= 2
+                elif self.x > x and self.y > y:
+                    self.x -= 2
+                    self.y -= 2
+                elif self.x < x and self.y < y:
+                    self.x += 2
+                    self.y += 2
+                elif self.x > x and self.y < y:
+                    self.x -= 2
+                    self.y += 2
         self.rect.topleft = self.x, self.y
 
     

5efe04bc9079263eabc77a963921d363bb2e5d6f
Gabriel Aragão <97701396+gabrielaragao01@users.noreply.github.com>
2022-11-03 23:20:47 -0300

    Update drops.py

--- drops.py
+++ drops.py
@@ -5,7 +5,7 @@ from random import randint
 
 
 def dropar_vida(x, y):
-    chance = randint(1, 100)
+    chance = randint(1, 10)
 
     lista_coordenadas_vida = [x, y]
 
@@ -18,7 +18,7 @@ def dropar_vida(x, y):
     return lista_coordenadas_vida
     
 def dropar_bota(x, y):
-    chance = randint(1, 100)
+    chance = randint(1, 10)
 
     lista_coordenadas_bota = [x, y]
 
@@ -30,3 +30,17 @@ def dropar_bota(x, y):
 
     lista_coordenadas_bota.append(drop2)
     return lista_coordenadas_bota
+
+def dropar_gun(x, y):
+    chance = randint(1, 10)
+
+    lista_coordenadas_gun = [x, y]
+
+    if chance == 3:
+        drop3 = True
+
+    else:
+        drop3 = False
+
+    lista_coordenadas_gun.append(drop3)
+    return lista_coordenadas_gun
